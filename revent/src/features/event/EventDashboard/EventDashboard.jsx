5b48bd0d1389e5ea0386c57959811043608e8d91
Joshua <joshualjohnson33@gmail.com>
2018-05-20 20:31:55 -0400

    form complete

--- revent/src/features/event/EventDashboard/EventDashboard.jsx
+++ revent/src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,75 +1,111 @@
-import React, { Component } from 'react'
-import { Grid, Button } from 'semantic-ui-react';
-import EventList from '../EventList/EventList';
-import EventForm from '../EventForm/EventForm';
+import React, { Component } from "react";
+import { Grid, Button } from "semantic-ui-react";
+import cuid from 'cuid';
+import EventList from "../EventList/EventList";
+import EventForm from "../EventForm/EventForm";
 
 const eventsDashboard = [
   {
-    id: '1',
-    title: 'Trip to Tower of London',
-    date: '2018-03-27T11:00:00+00:00',
-    category: 'culture',
+    id: "1",
+    title: "Trip to Tower of London",
+    date: "2018-03-27T11:00:00+00:00",
+    category: "culture",
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-    city: 'London, UK',
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.",
+    city: "London, UK",
     venue: "Tower of London, St Katharine's & Wapping, London",
-    hostedBy: 'Bob',
-    hostPhotoURL: 'http://randomuser.me/api/portraits/women/99.jpg',
+    hostedBy: "Bob",
+    hostPhotoURL: "http://randomuser.me/api/portraits/women/99.jpg",
     attendees: [
       {
-        id: 'a',
-        name: 'Bob',
-        photoURL: 'http://randomuser.me/api/portraits/women/97.jpg'
+        id: "a",
+        name: "Bob",
+        photoURL: "http://randomuser.me/api/portraits/women/97.jpg"
       },
       {
-        id: 'b',
-        name: 'Tom',
-        photoURL: 'http://randomuser.me/api/portraits/women/98.jpg'
+        id: "b",
+        name: "Tom",
+        photoURL: "http://randomuser.me/api/portraits/women/98.jpg"
       }
     ]
   },
   {
-    id: '2',
-    title: 'Trip to Punch and Judy Pub',
-    date: '2018-03-28T14:00:00+00:00',
-    category: 'drinks',
+    id: "2",
+    title: "Trip to Punch and Judy Pub",
+    date: "2018-03-28T14:00:00+00:00",
+    category: "drinks",
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-    city: 'London, UK',
-    venue: 'Punch & Judy, Henrietta Street, London, UK',
-    hostedBy: 'Tom',
-    hostPhotoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.",
+    city: "London, UK",
+    venue: "Punch & Judy, Henrietta Street, London, UK",
+    hostedBy: "Tom",
+    hostPhotoURL: "https://randomuser.me/api/portraits/men/22.jpg",
     attendees: [
       {
-        id: 'b',
-        name: 'Tom',
-        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
+        id: "b",
+        name: "Tom",
+        photoURL: "https://randomuser.me/api/portraits/men/22.jpg"
       },
       {
-        id: 'a',
-        name: 'Bob',
-        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
+        id: "a",
+        name: "Bob",
+        photoURL: "https://randomuser.me/api/portraits/men/20.jpg"
       }
     ]
   }
-]
-
+];
 
 class EventDashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      events: eventsDashboard,
+      isOpen: false
+    }
+
+
+  }
+
+  // arrow function does the binding for us
+  _handleCancel = () => {
+    this.setState({
+      isOpen: false
+    })
+  }
+
+  _handleFormOpen = () => {
+    this.setState({
+      isOpen: true
+    })
+  }
+
+  _handleCreateEvent = (newEvent) => {
+    newEvent.id = cuid();
+    newEvent.hostPhotoURL = 'assets/user.png';
+
+    const updatedEvents = [...this.state.events, newEvent];
+
+    this.setState({
+      events: updatedEvents,
+      isOpen: false
+    })
+  }
+
   render() {
     return (
       <Grid>
-        <Grid.Column width={ 10 }>
-            <EventList events={ eventsDashboard } />
+        <Grid.Column width={10}>
+          <EventList events={this.state.events} />
         </Grid.Column>
-        <Grid.Column width={ 6 }>
-        <Button positive content='Create Event' />
-            <EventForm />
+        <Grid.Column width={6}>
+          <Button onClick={this._handleFormOpen} positive content="Create Event" />
+          {this.state.isOpen && 
+          <EventForm createEvent={this._handleCreateEvent} _handleCancel={this._handleCancel} /> }
         </Grid.Column>
       </Grid>
-    )
+    );
   }
 }
-
 
 export default EventDashboard;
--- revent/src/features/event/EventForm/EventForm.jsx
+++ revent/src/features/event/EventForm/EventForm.jsx
@@ -2,34 +2,64 @@ import React, { Component } from "react";
 import { Segment, Form, Button } from 'semantic-ui-react';
 
 class EventForm extends Component {
+  state = {
+    event: {
+      title: "",
+      date: "",
+      city: "",
+      venue: "",
+      hostedBy: ""
+
+    }
+  }
+
+  _onFormSubmit = (event) => {
+    event.preventDefault();
+
+    // this.refs is a prop in component class
+    this.props.createEvent(this.state.event);
+  }
+
+  _onInputChange = (event) => {
+    const newEvent = this.state.event;
+    newEvent[event.target.name] = event.target.value;
+
+    this.setState({
+      event: newEvent
+    })
+  }
+
   render() {
+    const { _handleCancel } = this.props;
+    const { event } = this.state;
+
     return (
       <Segment>
-        <Form>
+        <Form onSubmit={this._onFormSubmit}>
           <Form.Field>
             <label>Event Title</label>
-            <input placeholder="First Name" />
+            <input name="title" onChange={this._onInputChange} value={event.title} placeholder="Event Title" />
           </Form.Field>
           <Form.Field>
             <label>Event Date</label>
-            <input type="date" placeholder="Event Date" />
+            <input name="date" onChange={this._onInputChange} value={event.date} type="date" placeholder="Event Date" />
           </Form.Field>
           <Form.Field>
             <label>City</label>
-            <input placeholder="City event is taking place" />
+            <input name="city" onChange={this._onInputChange} value={event.city} placeholder="City event is taking place" />
           </Form.Field>
           <Form.Field>
             <label>Venue</label>
-            <input placeholder="Enter the Venue of the event" />
+            <input name="venue" onChange={this._onInputChange} value={event.venue}  placeholder="Enter the Venue of the event" />
           </Form.Field>
           <Form.Field>
             <label>Hosted By</label>
-            <input placeholder="Enter the name of person hosting" />
+            <input name="hostedBy" onChange={this._onInputChange} value={event.hostedBy} placeholder="Enter the name of person hosting" />
           </Form.Field>
           <Button positive type="submit">
             Submit
           </Button>
-          <Button type="button">Cancel</Button>
+          <Button onClick={_handleCancel} type="button">Cancel</Button>
         </Form>
       </Segment>
     );
