82c9367c2d6a83e4216acca44714c70b37128d79
Athura <joshualjohnson33@gmail.com>
2018-06-13 07:52:33 -0400

    Begin firestore authentication, finished regular email/password login and register

--- revent/src/features/auth/authActions.jsx
+++ revent/src/features/auth/authActions.jsx
@@ -1,15 +1,45 @@
-import { LOGIN_USER, SIGN_OUT_USER } from './authConstants';
+import { SubmissionError } from 'redux-form';
 import { closeModal } from '../modals/modalActions';
 
 export const login = (creds) => {
-    return dispatch => {
-        dispatch({type: LOGIN_USER, payload: {creds}})
-        dispatch(closeModal())
+    return async (dispatch, getState, {getFirebase}) => {
+        const firebase = getFirebase();
+        try {
+            await firebase.auth().signInWithEmailAndPassword(creds.email, creds.password);
+            dispatch(closeModal())
+        } catch (error) {
+            console.log(error);
+            throw new SubmissionError({
+                _error: 'Login failed'
+            })
+        }     
     }
 }
 
-export const logout = () => {
-    return {
-        type: SIGN_OUT_USER
+export const registerUser = (user) => 
+    async (dispatch, getState, {getFirebase, getFirestore}) => {
+        const firebase = getFirebase();
+        const firestore = getFirestore();
+        try {
+            // create the user in auth
+            let createUser = await firebase.auth().createUserWithEmailAndPassword(user.email, user.password);
+            console.log(createUser);
+            // update the auth profile (in redux tools)
+            await createUser.updateProfile({
+                displayName: user.displayName
+            })
+            // create a new profile in firestore
+            let newUser = {
+                displayName: user.displayName,
+                createdAt: firestore.FieldValue.serverTimestamp()
+            };
+            await firestore.set(`users/${createUser.uid}`, {...newUser});
+            dispatch(closeModal());
+        } catch (error) {
+            console.log(error)
+            throw new SubmissionError({
+                _error: error.message
+            })
+        }
     }
-}
+
